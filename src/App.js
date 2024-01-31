24dbdde5d993ffe5a560438e03149deb590ff388
Abdullah Mohamed <3abdullah7mohamed@gmail.com>
2024-01-31 19:43:54 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import SideBar from './SideBar';
+import ChatList from './ChatList';
+import Chat from './Chat';
+import ChatBlock from './ChatBlock';
+import SignUpLogin from './SignUpLogin';
+import { useEffect } from 'react';
+import {useState } from 'react';
+import SignUp from './SignUp';
 
 function App() {
+  let nums=[1,2,3,4,5,3,3,3,3,3,3,3,3];
+  let chats=nums.map((m)=>{
+    return <ChatBlock/>
+  });
+  const [user, setUser] = useState(localStorage.getItem('currUser'));
+  let [signLogin,setSignLogin] = useState(false);
+  useEffect(()=>{
+    const handleStorageChange = () => {
+      setUser(localStorage.getItem('currUser'));
+      localStorage.removeItem('currUser');
+    };
+    window.addEventListener('storage', handleStorageChange);
+    return () => {
+      window.removeEventListener('storage', handleStorageChange);
+    };
+  },[])
+  function goSignUp(){
+    setSignLogin(!signLogin);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        {user===null &&!signLogin&& 
+        <div>
+          <button className='signlogin' onClick={goSignUp}>Sign up</button>
+          <SignUpLogin/>
+        </div>
+        }
+
+        {user===null &&signLogin&&
+        <div>
+          <button id='gologin' className='signlogin' onClick={goSignUp}>Login</button>
+          <SignUp/>
+        </div>  
+        }
+        {user!==null && 
+        <div className='logged'>
+          <SideBar/>
+          <ChatList chats={chats}/>
+          <Chat/> 
+        </div>}
+
     </div>
   );
 }
